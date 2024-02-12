a123057a5dabcef9fab76738342924b928247dfb
poomon001 <poomon2542@gmail.com>
2024-02-12 00:04:49 -0800

    test

---
+++ user/user.go
@@ -0,0 +1,10 @@
+package tester
+
+import ("fmt"
+"github.com/Poomon001/day-trading-package/package")
+
+func Tester() {
+	fmt.Println("User start:")
+	daytrading.TestMain()
+	fmt.Println("User end:")
+}
