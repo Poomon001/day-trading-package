62e87d1d6ea8001b0e0cef537eb9a5099d59405a
poomon001 <poomon2542@gmail.com>
2024-02-12 00:09:50 -0800

    fix bug

--- user/user.go
+++ user/user.go
@@ -3,7 +3,7 @@ package user
 import ("fmt"
 "github.com/Poomon001/day-trading-package/package")
 
-func user() {
+func TestUser() {
 	fmt.Println("User start:")
 	daytrading.TestMain()
 	fmt.Println("User end:")
