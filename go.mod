855f94dd9e56dfbddb2aa74e842219808b133e8e
poomon001 <poomon2542@gmail.com>
2024-02-11 21:51:53 -0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Poomon001/day-trading-package
+
+go 1.21.6
---
+++ package.go
@@ -0,0 +1,16 @@
+// TO CHANGE: version - v1.0.1
+package daytrading
+import ("fmt"
+"middleware/identification")
+
+func Identification(c *gin.Context) {
+	identification.IdentificationMiddleware(c)
+}
+
+func TestMain() {
+	fmt.Println("TestMain")
+}
+
+func TestIdentification() {
+	identification.Test()
+}
