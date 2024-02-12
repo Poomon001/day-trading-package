8d5f6b9fe254efa98a02bc5616157adbe2ee945f
poomon001 <poomon2542@gmail.com>
2024-02-11 23:36:04 -0800

    update

--- package/package.go
+++ package/package.go
@@ -1,13 +1,7 @@
 // TO CHANGE: version - v1.0.7
 package daytrading
-import ("fmt"
-"github.com/Poomon001/day-trading-package/test")
+import ("fmt")
 
 func TestMain() {
 	fmt.Println("TestMain")
-}
-
-func Test() {
-	tester.Test()
-	fmt.Println("Test")
 }
